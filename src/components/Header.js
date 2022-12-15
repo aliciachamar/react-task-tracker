10d9ed698dcb30f0c229dfe8008ad03dbe4a79a6
aliciachamar <aliciachamar@gmail.com>
2022-12-14 20:28:07 -0800

    added Button component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,10 @@
+import Button from "./Button";
+
 const Header = () => {
   return (
-    <header>
+    <header className="header">
       <h1>Task Tracker</h1>
+      <Button color="green" text="Hello"/>
     </header>
   )
 }
