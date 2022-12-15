ae076877866a3ff2722ad10438ca0120768fa4c2
aliciachamar <aliciachamar@gmail.com>
2022-12-14 20:34:13 -0800

    added onClick event to Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,13 @@
-const Button = ({ color, text }) => {
+const Button = ({ color, text, onClick }) => {
   return (
-    <button className="btn" style={{ backgroundColor: color }}>{text}</button>
+    <button 
+      className="btn" 
+      style={{ backgroundColor: color }}
+      onClick={onClick}
+    >
+      {text}
+    </button>
   )
 }
 
-export default Button
+export default Button;
