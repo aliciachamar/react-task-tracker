94d69637870111bbc09fe04198ef9d8005703d7f
aliciachamar <aliciachamar@gmail.com>
2022-12-27 07:48:01 -0800

    added dummy deleteTask function and no tasks to show functionality

--- src/App.js
+++ src/App.js
@@ -28,16 +28,18 @@ function App () {
 
   // Delete task
   const deleteTask = (id) => {
-    console.log("delete ", id);
+    setTasks(tasks.filter((task) => task.id !== id));
   }
 
   return (
     <div className="container">
       <Header />
-      <Tasks 
+      {tasks.length > 0 ? 
+        <Tasks 
         tasks={tasks} 
         onDelete={deleteTask}
-      />
+        /> : 
+        ("No tasks to show")}
     </div>
   );
 }
